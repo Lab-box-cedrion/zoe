3d904578361d72e3e7cd10102e33dba18ad59460
Javiuty <javiiescuadra@gmail.com>
2020-04-01 14:59:02 +0200

    navbar + routes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,23 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header>
-      <div className="container-header">
+      <div className="container-image">
         <img
           src="https://s5.gifyu.com/images/icono-cedrion.png"
           alt="main-icono"
         />
-        </div>
-        <div className="container-menu">
-          <div className="menu">INFO</div>
-          <div className="menu">AJUSTES</div>
-          <div className="menu">HISTÓRICOS</div>
-          <div className="menu">MAX/MIN</div>
-        </div>
-      
+      </div>
+
+      <ul className="container-menu">
+        <li className="menu">INFO</li>
+        <li className="menu">AJUSTES</li>
+        <li className="menu">HISTÓRICOS</li>
+        <li className="menu">MAX/MIN</li>
+      </ul>
     </header>
   );
 };
---
+++ src/Components/Routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+
+const Routes=()=>{
+
+  return(
+    <div>
+      <Route path="/" exact component={} />
+      <Route path="ajustes" component={} />
+      <Route path="historicos" component={} />
+      <Route path="notas" component={} />
+      <Route path="max-min" component={} />
+    </div>
+
+  )
+
+}
+
+export default Routes

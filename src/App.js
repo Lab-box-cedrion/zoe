3b99c53070d3688a0e58bc0e4eeff69181c2fab2
Leired7 <leirediez.7@gmail.com>
2020-04-05 20:36:38 +0200

    Parameters form (v.0.0)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from "react";
 import Cabecera from "./Components/Cabecera/Cabecera";
-import Pie from "./Components/Footer/Pie"
-
-
+import Pie from "./Components/Footer/Pie";
+import SelectParameters from "./Components/SelectParameters/SelectParameters";
 
 function App() {
   return (
     <Fragment>
       <Cabecera />
-
-
-
+      <SelectParameters />
       <Pie />
     </Fragment>
-      
   );
 }
 

e5ce5c54006e609e45a785438c4618a742650cff
solar <jarabelmonte@gmail.com>
2020-04-05 16:49:39 +0200

    cambio dark

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react';
 import Cabecera from "./Components/Cabecera/Cabecera";
-import Pie from "./Components/Footer/Pie"
+import Pie from "./Components/Footer/Pie";
+import './App.css';
 
 
 

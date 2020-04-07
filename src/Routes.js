a39de8f4544a2d22251b05220afa469e50f5901a
Leired7 <leirediez.7@gmail.com>
2020-04-07 13:50:26 +0200

    Import Home and SelectParameters components, wrap divs of return in Router component

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import SelectParameters from "./SelectParameters/SelectParameters";
+import SelectParameters from "./Components/SelectParameters/SelectParameters";
+import Home from "./Components/Home/Home";
 
 const Routes = () => {
   return (
-    <div>
-      <Route path="/" component={Home} />
-      <Route path="/info" component={} />
-      <Route path="/ajustes" component={SelectParameters} />
-      <Route path="historicos" component={} />
+    <Router>
+      <div>
+        <Route path="/" exact component={Home} />
+        {/*       <Route path="/info" component={} />
+         */}{" "}
+        <Route path="/ajustes" component={SelectParameters} />
+        {/* <Route path="historicos" component={} />
       <Route path="notas" component={} />
-      <Route path="max-min" component={} />
-    </div>
+      <Route path="max-min" component={} /> */}
+      </div>
+    </Router>
   );
 };
 

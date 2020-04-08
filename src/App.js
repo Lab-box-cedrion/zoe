ed3c2aa1925245fe8d769cd46a4e841849fb9266
Leired7 <leirediez.7@gmail.com>
2020-04-08 13:23:51 +0200

    Deleted divs in Routes.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Cabecera from "./Components/Cabecera/Cabecera";
 import Pie from "./Components/Footer/Pie";
 import "./App.css";
 import Home from "./Components/Home/Home";
 import Routes from "./Routes";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Routes />
-    </Router>
+    <React.Fragment>
+      <Router>
+        <Routes />
+      </Router>
+    </React.Fragment>
   );
 }
 
--- src/Routes.js
+++ src/Routes.js
@@ -6,15 +6,13 @@ import Home from "./Components/Home/Home";
 const Routes = () => {
   return (
     <Router>
-      <div>
-        <Route path="/" exact component={Home} />
-        {/*       <Route path="/info" component={} />
-         */}{" "}
-        <Route path="/ajustes" component={SelectParameters} />
-        {/* <Route path="historicos" component={} />
+      <Route path="/" exact component={Home} />
+      {/*       <Route path="/info" component={} />
+       */}{" "}
+      <Route path="/ajustes" component={SelectParameters} />
+      {/* <Route path="historicos" component={} />
       <Route path="notas" component={} />
       <Route path="max-min" component={} /> */}
-      </div>
     </Router>
   );
 };

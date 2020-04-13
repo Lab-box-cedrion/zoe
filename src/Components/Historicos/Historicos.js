962900fea0370f4266f08a9245128707f7a80967
solar <jarabelmonte@gmail.com>
2020-04-13 23:21:31 +0200

    chart con navegador y footer

--- src/Components/Historicos/Historicos.js
+++ src/Components/Historicos/Historicos.js
@@ -1,13 +1,16 @@
 import React, { Component, Fragment } from 'react';
 import LineChartDemo from './Charts';
-
+import Cabecera from '../Cabecera/Cabecera';
+import Pie from '../Footer/Pie';
 
 class Historicos extends Component {
    
     render() { 
         return ( 
             <Fragment>
+                <Cabecera />
                 <LineChartDemo />
+                <Pie />
             </Fragment>
             
          );

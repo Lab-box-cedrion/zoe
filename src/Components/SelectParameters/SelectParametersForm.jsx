d866d7f7c49289a530b5d9f00904ccb1e8691488
Leired7 <leirediez.7@gmail.com>
2020-04-15 08:30:07 +0200

    Created new form to select parameters

---
+++ src/Components/SelectParameters/SelectParametersForm.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+import "./SelectParametersForm.scss";
+
+import { Container, Row, Col } from "react-bootstrap";
+
+function SelectParametersForm() {
+  return (
+    <React.Fragment>
+      <Container>
+        <Row>
+          <Col xs={12} className="intro">
+            <h1>Selección de parámetros</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+              nihil nesciunt sequi libero accusamus dolore corrupti autem
+              maxime, perspiciatis incidunt, et, necessitatibus soluta
+              laboriosam dolores sit vitae nobis amet sint?
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <form className="form-parameters">
+              <label>
+                <input type="date"></input>
+                <input type="text" placeholder="Nombre"></input>
+              </label>
+              <label className="tiempo">
+                <input
+                  type="number"
+                  name="tiempo"
+                  placeholder="Tiempo h:mm:ss"
+                  value="hola"
+                ></input>
+                <button className="btn plus ">&#9650;</button>
+                <button className="btn minus">&#9660;</button>
+              </label>
+              <label className="tension">
+                <input
+                  type="number"
+                  name="tension"
+                  placeholder="Tension KV"
+                  value="hola"
+                ></input>
+                <button className="btn plus ">&#9650;</button>
+                <button className="btn minus">&#9660;</button>
+              </label>
+              <label className="amperaje">
+                <input
+                  type="number"
+                  name="amperaje"
+                  placeholder="Amperaje mA"
+                  value="hola"
+                ></input>
+                <button className="btn plus ">&#9650;</button>
+                <button className="btn minus">&#9660;</button>
+              </label>
+              <label className="temperatura">
+                <input
+                  type="number"
+                  name="temperatura"
+                  placeholder="Temperatura ºC"
+                  value="hola"
+                ></input>
+                <button className="btn plus ">&#9650;</button>
+                <button className="btn minus">&#9660;</button>
+              </label>
+              <label className="humedad">
+                <input
+                  type="number"
+                  name="humedad"
+                  placeholder="Humedad %"
+                  value="hola"
+                ></input>
+                <button className="btn plus ">&#9650;</button>
+                <button className="btn minus">&#9660;</button>
+              </label>
+              <label className="luz-pulsada">
+                <input
+                  type="number"
+                  name="luzPulsada"
+                  placeholder="Temperatura ºC"
+                  value="hola"
+                ></input>
+                <button className="btn plus ">&#9650;</button>
+                <button className="btn minus">&#9660;</button>
+              </label>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default SelectParametersForm;
--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import SelectParameters from "./Components/SelectParameters/SelectParameters";
+import SelectParametersForm from "./Components/SelectParameters/SelectParametersForm";
 import Home from "./Components/Home/Home";
 import Historicos from "./Components/Historicos/Historicos";
 import H_grafica from "./Components/Historicos/H_grafica";
@@ -12,7 +12,7 @@ const Routes = () => {
   return (
     <Router>
       <Route path="/" exact component={Home} />
-      <Route path="/ajustes" component={SelectParameters} />
+      <Route path="/ajustes" component={SelectParametersForm} />
       <Route path="/historicos" component={Historicos} />
       <Route path="/historicos_grafica" component={H_grafica} />
       <Route path="/test" component={Testeo} />

efaa0f75d7da7915a81386956162aa14bab2a243
Javiuty <javiiescuadra@gmail.com>
2020-04-15 14:04:13 +0200

    Fav Icon, new Brand-Icon, FontAwesome dependencies

--- src/Components/Cabecera/Cabecera.js
+++ src/Components/Cabecera/Cabecera.js
@@ -14,10 +14,10 @@ const Cabecera = () => {
     <div id="cabecera">
       <img
         className="brand-icon"
-        src="https://s6.gifyu.com/images/logo-cedrion-dark-trans-01.md.png"
+        src="https://s6.gifyu.com/images/cedrion_horizontal_dark_TRANS-01.png"
         alt="main-icono"
       />
-      <Navbar collapseOnSelect expand="sm">
+      <Navbar collapseOnSelect expand="md">
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
--- src/Testeo.js
+++ src/Testeo.js
@@ -1,13 +1,19 @@
 import React, { Fragment } from 'react';
 import Cabecera from './Components/Cabecera/Cabecera';
 import Pie from "./Components/Footer/Pie";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCog} from "@fortawesome/free-solid-svg-icons";
 
 
 const Testeo = () => {
+
+  let iconoAjustes = <FontAwesomeIcon icon={faCog} />
+ 
+
   return (  
     <Fragment>
       <Cabecera />
-      <h4>Página de testeo</h4>
+  <h4>Página de testeo {iconoAjustes}</h4>
       <Pie />
     </Fragment>
   ); 

3ae6d3e3ced314e4e6220e24934f2880d90d354b
solar <jarabelmonte@gmail.com>
2020-04-17 15:06:35 +0200

    nuevo planteamiento historicos mini componentes

--- src/Components/Historicos/DateBox.js
+++ src/Components/Historicos/DateBox.js
@@ -1,17 +1,17 @@
 import React, { Component, Fragment } from 'react';
-import {Fieldset} from 'primereact/fieldset';
+
 
 
 export default class DateBox extends Component {
     
 
-    let date = new Date();
+    // let date = new Date();
     
     render() { 
         return ( 
             <Fragment>
                 <article>
-                    <p>{date}</p>
+                    {/* <p>{date}</p> */}
                 </article>
             </Fragment>    
          );

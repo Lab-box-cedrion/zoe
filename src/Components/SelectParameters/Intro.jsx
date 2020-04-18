a6264bdbaadd81e6bf81b67305dd44438e4adc42
Leired7 <leirediez.7@gmail.com>
2020-04-18 19:13:04 +0200

    Working on styles and state in form

---
+++ src/Components/SelectParameters/Intro.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Intro = () => (
+  <>
+    <h1>Selección de parámetros</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam nihil
+      nesciunt sequi libero accusamus dolore corrupti autem maxime, perspiciatis
+      incidunt, et, necessitatibus soluta laboriosam dolores sit vitae nobis
+      amet sint?
+    </p>
+  </>
+);
+
+export default Intro;
--- src/Components/SelectParameters/SelectParametersForm.jsx
+++ src/Components/SelectParameters/SelectParametersForm.jsx
@@ -2,20 +2,31 @@ import React, { useState } from "react";
 import Cabecera from "../Cabecera/Cabecera";
 import Pie from "../Footer/Pie";
 
+import Intro from "./Intro";
+
+import { InputText } from "primereact/inputtext";
 import "./SelectParametersForm.scss";
 
 function SelectParametersForm() {
   // contiene los nombres de los campos de los input
-  const [date, setDate] = useState(new Date());
+  const date = new Date();
+  var options = {
+    weekday: "short",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  date.toLocaleString("es-Es", options);
+  const [name, setName] = useState("");
+  const [time, setTime] = useState(null);
+  const [tension, setTension] = useState(null);
+  const [amperage, setAmperage] = useState(null);
+  const [temperature, setTemperature] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [pulsedLight, setpulsedLight] = useState("true");
 
-  /* tension: "",
-    amperaje: "",
-    temperatura: "",
-    humedad: "",
-    luzpulsada: false,
+  /* 
     microorganismos: "", */
-
-  const [tiempo, setTiempo] = useState(0);
 
   const submitInfo = (event) => {
     event.preventDefault();
@@ -25,123 +36,101 @@ function SelectParametersForm() {
   return (
     <React.Fragment>
       <Cabecera />
-      <section className="containerCSS">
-        <h1>Selección de parámetros</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-          nihil nesciunt sequi libero accusamus dolore corrupti autem maxime,
-          perspiciatis incidunt, et, necessitatibus soluta laboriosam dolores
-          sit vitae nobis amet sint?
-        </p>
+
+      <section>
+        <Intro />
         <form
           onSubmit={(event) => submitInfo(event)}
-          className=" form-parameters"
+          className=" containerCss form-parameters"
         >
-          <div className="fecha">
+          <div className="date">
             <label htmlFor="fecha">Fecha</label>
             <input
               type="input"
+              className="input"
               name="fecha"
               value={date}
               placeholder={date}
             ></input>
           </div>
-          <div>
-            <label>Nombre</label>
-            <input
-              type="text"
-              placeholder="Nombre (opcional)"
-              name="nombre"
-              value="nombre"
-            ></input>
+          <div className="name">
+            <span className="p-float-label">
+              <InputText
+                id="float-input"
+                className="input"
+                type="text"
+                size="30"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <label htmlFor="float-input">Nombre (opcional)</label>
+            </span>
           </div>
-
-          <label htmlFor="tiempo" className="tiempo" value={tiempo}>
-            Tiempo
-          </label>
-          <input
-            type="number"
-            name="tiempo"
-            placeholder="Tiempo h:mm:ss"
-            value={tiempo}
-            onChange={(event) => setTiempo(event.target.value)}
-          ></input>
-          <button
-            className="button plus "
-            onClick={() => setTiempo(tiempo + 10)}
-          >
-            &#9650;
-          </button>
-          <button
-            className="button minus"
-            onClick={() => setTiempo(tiempo - 10)}
-          >
-            &#9660;
-          </button>
-          {/* <label htmlFor="tensión" className="tension" value={newExperiment.tension}>
-            <input
-              type="number"
-              name="tension"
-              placeholder="Tension KV"
-              value="hola"
-            ></input>
-            <button className="button plus ">&#9650;</button>
-            <button className="button minus">&#9660;</button>
-          </label>
-          <label htmlFor="amperaje" className="amperaje">
-            <input
-              type="number"
-              name="amperaje"
-              placeholder="Amperaje mA"
-              value="hola"
-            ></input>
-            <button className="button plus ">&#9650;</button>
-            <button className="button minus">&#9660;</button>
-          </label>
-          <label htmlFor="temperatura" className="temperatura">
-            <input
-              type="number"
-              name="temperatura"
-              placeholder="Temperatura ºC"
-              value="hola"
-            ></input>
-            <button className="button plus ">&#9650;</button>
-            <button className="button minus">&#9660;</button>
-          </label>
-          <label htmlFor="humedad" className="humedad">
-            <input
-              type="number"
-              name="humedad"
-              placeholder="Humedad %"
-              value="hola"
-            ></input>
-            <button className="button plus ">&#9650;</button>
-            <button className="button minus">&#9660;</button>
-          </label>
-          <label htmlFor="luzpulsada" className="luz-pulsada">
-            Luz pulsada
-            {/* <input
-              type="number"
-              name="luzPulsada"
-              placeholder="luzPulsada"
-              value="hola"
-            ></input> 
-            <button className="button plus ">Sí</button>
-            <button className="button minus">No</button>
-          </label>
-          <select id="microorganismos" name="microorganismos">
-            <option value="Pseudomonas aeruginosa">
-              Pseudomonas aeruginosa
-            </option>
-            <option value="Listeria Monocytogenes">
-              Listeria Monocytogenes
-            </option>
-            <option value="Escherichia Coli">Escherichia Coli</option>
-            <option value="Staphylococcus aureus">Staphylococcus aureus</option>
-          </select> */}
-          <button className="button" type="submit">
-            Aplicar parámetros
-          </button>
+          <div className="time">
+            <span className="p-float-label">
+              <InputText
+                id="float-input"
+                className="input"
+                type="text"
+                size="30"
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
+              />
+              <label htmlFor="float-input">Tiempo h:m:s</label>
+            </span>
+          </div>
+          <div className="tension">
+            <span className="p-float-label">
+              <InputText
+                id="float-input"
+                className="input"
+                type="text"
+                size="30"
+                value={tension}
+                onChange={(e) => setTension(e.target.value)}
+              />
+              <label htmlFor="float-input">Tensión Kv</label>
+            </span>
+          </div>
+          <div className="amperage">
+            <span className="p-float-label">
+              <InputText
+                id="float-input"
+                className="input"
+                type="text"
+                size="30"
+                value={amperage}
+                onChange={(e) => setAmperage(e.target.value)}
+              />
+              <label htmlFor="float-input">Amperaje mA</label>
+            </span>
+          </div>
+          <div className="temperature">
+            <span className="p-float-label">
+              <InputText
+                id="float-input"
+                className="input"
+                type="text"
+                size="30"
+                value={temperature}
+                onChange={(e) => setTemperature(e.target.value)}
+              />
+              <label htmlFor="float-input">Temperatura ºC</label>
+            </span>
+          </div>
+          <div className="humidity">
+            <span className="p-float-label">
+              <InputText
+                id="float-input"
+                className="input"
+                type="text"
+                size="30"
+                value={humidity}
+                onChange={(e) => setHumidity(e.target.value)}
+              />
+              <label htmlFor="float-input">Humedad %</label>
+            </span>
+          </div>
         </form>
       </section>
       <Pie />

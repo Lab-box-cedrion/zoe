cebcfd5032ee7400651fd0b96f0e1e8bfdd35cb2
Javiuty <javiiescuadra@gmail.com>
2020-04-21 09:46:52 +0200

    small changes

--- src/Components/Notas/Notas.js
+++ src/Components/Notas/Notas.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Cabecera from "../Cabecera/Cabecera";
 import Pie from "../Footer/Pie";
+import "./Notas.css";
+
 
 const Notas = () => {
+
+ 
+
   return (  
-    <div> 
+    <Fragment> 
       <Cabecera />
+        <div className="container-notas">
+          <input type="text" id="buscar" name="buscar" placeholder="Buscar" />
+          <div className="fecha-proyecto">dd-mm-aa</div>
+          <div className="nombre-notas">Proyecto 1</div>
+        </div>
 
       <Pie />
 
-    </div>
+    </Fragment>
   );
 }
  

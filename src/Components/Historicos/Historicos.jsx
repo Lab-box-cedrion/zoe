96d29f3d0713a4e3f0290518d93959592ac6cfc2
Leired7 <leirediez.7@gmail.com>
2020-05-08 11:22:59 +0200

    form to iniciate arduino

--- src/Components/Historicos/Historicos.jsx
+++ src/Components/Historicos/Historicos.jsx
@@ -4,60 +4,92 @@ import Cabecera from '../Cabecera/Cabecera';
 import Pie from '../Footer/Pie';
 import './Historicos.scss';
 import axios from 'axios';
+import { InputText } from "primereact/inputtext";
+
 
 
 
 const Historicos = () => {
     // set up the hook. "data" is a variable used to store data, setData the function we call to insert stuff into "data"
     const [data, setData] = useState([]);
+    const [form, setValue] = useState({
+        puerto: "",
+        tiempo: null,
+        nombre: "",
+    });
     // fetch the data from the API, using axios, and call the function setData to store the JSON inside "data" variable
     useEffect(() => {
         async function fetchData() {
             const result = await axios.get(
                 'http://localhost:5005/graphic-data',
-              );
-      
-              setData(result.data);
-              console.log("Connected to the database", result.data);
+            );
+
+            setData(result.data);
+            console.log("Connected to the database", result.data);
         }
         fetchData();
-      }, []);
-      
-      
-        return ( 
-            <Fragment>
-                <Cabecera />
-                    <article className= 'content'>
-                        <h1 className= 'title'>
-                        Históricos
+    }, []);
+
+
+    return (
+        <Fragment>
+            <Cabecera />
+            <article className='content'>
+                <h1 className='title'>
+                    Históricos
                         </h1>
-                        <p className= 'about-historicos'>Archivo de  los experimentos. Se puede buscar por día y hora o nombre elegido. Ordenados de más reciente a más antiguo.  Seleccionando uno de  los campos se accederá a  una vista resumen de los resultados y seleccionando cada uno de ellos a  su vez  se  visualiza la gráfica de los resultados.</p>
-                    </article>
-                    <main className= 'data-container'>
-                        {!data ? 
-                            <div>Loading...</div>
-                            :
-                            data.map((el, index) => {
-                                return(
-                                     <Link to={{pathname: `/historicos_grafica/${el.experiment}`,state: {
-                                         data: data[index]
-                                     }}}>
-                                        <article className='pair' key={el.id}>
-                                            <section className= 'date-time'>{el.experiment}
+                <p className='about-historicos'>Archivo de  los experimentos. Se puede buscar por día y hora o nombre elegido. Ordenados de más reciente a más antiguo.  Seleccionando uno de  los campos se accederá a  una vista resumen de los resultados y seleccionando cada uno de ellos a  su vez  se  visualiza la gráfica de los resultados.</p>
+            </article>
+            <article>
+                <form className="ardu-form">
+                    <span className="p-float-label">
+                        <InputText id="in" value={form.puerto} onChange={(e) => setValue({ ...form, puerto: e.target.value })} />
+                        <label htmlFor="in">Puerto</label>
+                    </span>
+                    <span className="p-float-label">
+                        <InputText id="in" value={form.tiempo} onChange={(e) => setValue({ ...form, tiempo: e.target.value })} />
+                        <label htmlFor="in">Tiempo(s)</label>
+                    </span>
+                    <span className="p-float-label">
+                        <InputText id="in" value={form.nombre} onChange={(e) => setValue({ ...form, nombre: e.target.value })} />
+                        <label htmlFor="in">Nombre experimento</label>
+                    </span>
+                    <button
+                        className=""
+                        type="submit"
+                    >
+                        <span>Iniciar experimento</span>{" "}
+                    </button>
+                </form>
+            </article>
+
+            <main className='data-container'>
+                {!data ?
+                    <div>Loading...</div>
+                    :
+                    data.map((el, index) => {
+                        return (
+                            <Link to={{
+                                pathname: `/historicos_grafica/${el.experiment}`, state: {
+                                    data: data[index]
+                                }
+                            }}>
+                                <article className='pair' key={el.id}>
+                                    <section className='date-time'>{el.experiment}
+                                    </section>
+                                    <section className='name'>Nombre
                                             </section>
-                                            <section className='name'>Nombre
-                                            </section>
-                                        </article>
-                                    </Link>
-                                )
-                            })
-                        }
-                    </main>
-                <Pie />
-            </Fragment>
-            
-         );
-    
+                                </article>
+                            </Link>
+                        )
+                    })
+                }
+            </main>
+            <Pie />
+        </Fragment>
+
+    );
+
 }
- 
+
 export default Historicos;

c37a084cef6c40a2c4bb83b4222323b3f7b37679
solar <jarabelmonte@gmail.com>
2020-05-11 15:11:22 +0200

    haciendo responsive experimento

--- src/Components/Historicos/Historicos.jsx
+++ src/Components/Historicos/Historicos.jsx
@@ -73,7 +73,7 @@ const Historicos = () => {
                         </h1>
                 <p className='about-historicos'>Archivo de  los experimentos. Se puede buscar por día y hora o nombre elegido. Ordenados de más reciente a más antiguo.  Seleccionando uno de  los campos se accederá a  una vista resumen de los resultados y seleccionando cada uno de ellos a  su vez  se  visualiza la gráfica de los resultados.</p>
             </article>
-            <article>
+            <article className='submission'>
                 <form onSubmit={(event) => { enviarDatos(event) }} className="ardu-form">
                     <span className="p-float-label portfield">
                         <InputText id="in" value={form.puerto} onChange={(e) => setValue({ ...form, puerto: e.target.value })} />
@@ -110,13 +110,9 @@ const Historicos = () => {
                                 <article className='pair' key={el.id}>
                                     <section className='date-time'>{el.experiment}
                                     </section>
-                                    <section className='name'>{el.nombre.length === 0
-                                    
-                                        ? "---"
-                                        : el.nombre
-
-                                    }
-                                            </section>
+                                    <section className='name'>{el.nombre.length === 0 ? 
+                                    "---": el.nombre}
+                                    </section>
                                 </article>
                             </Link>
                         )

d326300576b0ca644d45d42f6307ed31aa946ad2
Leired7 <leirediez.7@gmail.com>
2020-05-23 16:13:43 +0200

    función ultimos datos para hparameters

--- src/Components/Historicos/HParametros.js
+++ src/Components/Historicos/HParametros.js
@@ -6,56 +6,69 @@ import "./HParametros.css";
 import { faTeeth } from "@fortawesome/free-solid-svg-icons";
 
 const HParametros = () => {
-
-
   const [lastExperiment, setLastExperiment] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch("http://localhost:5005/ultimo-experimento")
-      .then((data)=>{
-        return data.json()
+      .then((data) => {
+        return data.json();
       })
-      .then((dataJSON)=>{
-        setLastExperiment(dataJSON[0])
-        
-      })
+      .then((dataJSON) => {
+        setLastExperiment(dataJSON);
+        setLoading(true);
+      });
+  }, []);
 
-  },[])
+  const results = [];
 
-  // function lastResults() {
-  //   let stringHumidity = lastExperiment.humidity
-  //   // const arrayHumidity = stringHumidity.length
-  //   return console.log(stringHumidity);
-  // }
+  function lastResults() {
+    if (lastExperiment.length > 0) {
+      let arrayHumidity = lastExperiment[0].humidity.split(",");
+      let arrayTemperature = lastExperiment[0].temperature.split(",");
+      let arrayOzone = lastExperiment[0].ozone.split(",");
 
-  // lastResults();
-  
+      let lastResultsHUM = arrayHumidity[arrayHumidity.length - 1];
+      let lastResultsTem = arrayTemperature[arrayTemperature.length - 1];
+      let lastResultsOzo = arrayOzone[arrayOzone.length - 1],
+      return  console.log(lastResultsHUM);
 
+       
+    }
+  }
+  lastResults();
 
-  return (
+  console.log(lastExperiment);
+
+  return loading ? (
     <Fragment>
       <Cabecera />
-
       <div className="primer-container">
-        <div className="primer-item">{lastExperiment.experiment}</div>
-  <div className="primer-item">{lastExperiment.nombre}</div>
+        <div className="primer-item">{lastExperiment[0].experiment}</div>
+        <div className="primer-item">{lastExperiment[0].nombre}</div>
       </div>
 
       <div className="segundo-container">
         <div className="segundo-item">
-         <div className="paracircle"><p>23</p></div>
+          <div className="paracircle">
+            <p>23</p>
+          </div>
           <div className="parameter">
             <p>HR %</p>
           </div>
         </div>
         <div className="segundo-item">
-          <div className="paracircle"><p>25,7</p></div>
+          <div className="paracircle">
+            <p>25,7</p>
+          </div>
           <div className="parameter">
             <p>T ºC</p>
           </div>
         </div>
         <div className="segundo-item">
-          <div className="paracircle desactivado"><p>15,9</p></div>
+          <div className="paracircle desactivado">
+            <p>15,9</p>
+          </div>
           <div className="parameter disabled">
             <p>NOx %</p>
           </div>
@@ -64,19 +77,25 @@ const HParametros = () => {
 
       <div className="tercer-container">
         <div className="tercer-item">
-          <div className="paracircle"><p>0,9</p></div>
+          <div className="paracircle">
+            <p>0,9</p>
+          </div>
           <div className="parameter">
             <p>03 %</p>
           </div>
         </div>
         <div className="tercer-item">
-          <div className="paracircle desactivado"><p>77,8</p></div>
+          <div className="paracircle desactivado">
+            <p>77,8</p>
+          </div>
           <div className="parameter disabled">
             <p>H202 %</p>
           </div>
         </div>
         <div className="tercer-item">
-          <div className="paracircle desactivado"><p>34,6</p></div>
+          <div className="paracircle desactivado">
+            <p>34,6</p>
+          </div>
           <div className="parameter disabled">
             <p>X %</p>
           </div>
@@ -85,6 +104,8 @@ const HParametros = () => {
 
       <Pie />
     </Fragment>
+  ) : (
+    <div>loading</div>
   );
 };
 export default HParametros;

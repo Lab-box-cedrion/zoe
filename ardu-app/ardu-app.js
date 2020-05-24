b0def272c21ea83254aca351e588709ee27bb71e
Leired7 <leirediez.7@gmail.com>
2020-05-24 09:30:50 +0200

    limpieza segunda ardu-app.js y package.json

--- ardu-app/ardu-app.js
+++ ardu-app/ardu-app.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 5005;
 require("dotenv").config();
 const database = require("./conf");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Middleware para no tener problemas con los CORS cuando hagamos peticiones a nuestra API en Heroku
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
